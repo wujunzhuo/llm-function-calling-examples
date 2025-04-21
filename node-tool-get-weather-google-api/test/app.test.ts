eb31788219756029ad9cf7d54cd27b8186dd3d6b
C.C. <fanweixiao@gmail.com>
2025-04-21 22:37:18 +0800

    refactory: nodejs demo (#15)

--- node-tool-get-weather-google-api/test/app.test.ts
+++ node-tool-get-weather-google-api/test/app.test.ts
@@ -1,11 +1,11 @@
 import { expect, test } from "bun:test";
-import { getGeocode, getWeather } from "./app";
+import { getGeocode, getWeather } from "../src/app";
 
 test('getGeocode', async () => {
 	const address = '1600 Amphitheatre Parkway, Mountain View, CA';
 	const result = await getGeocode(address);
-	expect(result.lat).toBe(37.4193295);
-	expect(result.lng).toBe(-122.0816532);
+	expect(result.lat).toBeCloseTo(37.4193295);
+	expect(result.lng).toBeCloseTo(-122.0816532);
 })
 
 test('getWeatherByGoogleAPI', async () => {
